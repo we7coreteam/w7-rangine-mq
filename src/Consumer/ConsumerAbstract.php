d6aa936cdd6767e0e8c6e9155968c25279212882
titrxw <1120309488@qq.com>
2022-03-23 16:23:53 +0800

    优化代码

--- src/Consumer/ConsumerAbstract.php
+++ src/Consumer/ConsumerAbstract.php
@@ -129,9 +129,8 @@ abstract class ConsumerAbstract {
 		// If we're able to pull a job off of the stack, we will process it and then return
 		// from this method. If there is no job on the queue, we will "sleep" the worker
 		// for the specified number of seconds, then keep processing jobs after sleep.
-		if ($job) {
-			$this->runJob($job, $connectionName, $options);
-		}
+
+		$this->runJob($job, $connectionName, $options);
 
 		$this->resetTimeoutHandler($timerId);
 		$this->stopIfNecessary($options, time());
