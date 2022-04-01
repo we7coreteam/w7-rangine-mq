8fcac6cc921b0bf8f05654f3868e39d3bf55c969
titrxw <1120309488@qq.com>
2022-04-01 16:35:38 +0800

    修改连接释放

--- src/Consumer/RabbitMQConsumer.php
+++ src/Consumer/RabbitMQConsumer.php
@@ -106,7 +106,11 @@ class RabbitMQConsumer extends ConsumerAbstract {
 				$this->channel->wait();
 			} catch (Throwable $exception) {
 				$this->exceptions->report($exception);
+				break;
 			}
 		}
+
+		$this->channel->close();
+		$connection->close();
 	}
 }
